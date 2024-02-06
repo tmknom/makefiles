1e4f43839f70c028e28c58e3bf8c6cd8f3983570
tmknom <tmk.nom@gmail.com>
2024-02-06 11:53:18 +0900

    refactor: add yaml component

---
+++ internal/components/yaml.mk
@@ -0,0 +1,34 @@
+# YAML
+#
+# Requirements:
+# - Include `variables.mk`
+#
+# Usage:
+#
+# ```makefile
+# YAMLLINT_CONFIG ?= .yamllint.yml
+# -include $(COMPONENTS_DIR)/yaml.mk
+#
+# .PHONY: lint
+# lint: lint/yaml
+#
+# .PHONY: fmt
+# fmt: fmt/yaml
+# ```
+
+# Override variables
+YAMLLINT_CONFIG ?=
+
+# Variables
+YAMLLINT ?= ghcr.io/tmknom/dockerfiles/yamllint:latest
+PRETTIER ?= ghcr.io/tmknom/dockerfiles/prettier:latest
+YAML_FILES ?= $(shell find . -name '*.y*ml' | grep -v -e '.makefiles/' -e 'tmp/' -e '.git/')
+
+# Targets
+.PHONY: lint/yaml
+lint/yaml:
+	$(SECURE_DOCKER_RUN) $(YAMLLINT) --strict --config-file $(YAMLLINT_CONFIG) . || true
+
+.PHONY: fmt/yaml
+fmt/yaml:
+	$(SECURE_DOCKER_RUN) $(PRETTIER) --write --parser=yaml $(YAML_FILES)
